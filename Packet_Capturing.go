50c085546fd91521c26c38bc9d796f16991f55fe
borbo98 <bor1bogozalec@gmail.com>
2022-10-16 13:03:34 +0200

    First commit

---
+++ Packet_Capturing.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+)
+
+var (
+	iface    = "eth0"
+	devFound = false
+	snaplen  = int32(1600)
+	timeout  = pcap.BlockForever
+	filter   = "tcp and port 80"
+	promisc  = false
+)
+
+func main() {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Panicln(err)
+	}
+	for _, dev := range devices {
+		if dev.Name == iface {
+			devFound = true
+		}
+		if !devFound {
+			log.Panicf("device not found %s", iface)
+		}
+		handle, err := pcap.OpenLive(iface, snaplen, promisc, timeout)
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		defer handle.Close()
+
+		err = handle.SetBPFFilter(filter)
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		src := gopacket.NewPacketSource(handle, handle.LinkType())
+
+		for pkt := range src.Packets() {
+			appLayer := pkt.ApplicationLayer()
+			//skip if empty
+			if appLayer == nil {
+				continue
+			}
+			payload := appLayer.Payload()
+			search_arr := []string{"name", "username", "pass"}
+			//ignore index _
+			for _, s := range search_arr {
+				index := strings.Index(string(payload), s)
+				if index != -1 {
+					fmt.Println(string(payload[index : index+100]))
+				}
+			}
+		}
+	}
+}
